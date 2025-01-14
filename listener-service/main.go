df2924072c379ca3aff7b0c0aca8bc09084d4f18
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-01-14 14:56:09 +0200

    55. Change the RabbitMQ server URL to the Docker address

--- listener-service/main.go
+++ listener-service/main.go
@@ -41,7 +41,7 @@ func connect() (*amqp.Connection, error) {
 
 	// do not continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
 			fmt.Println(fmt.Errorf("RabbitMQ not yet ready: %w", err))
 			counts++
